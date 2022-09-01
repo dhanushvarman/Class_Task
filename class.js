019dbca3751ea8aa7dff17138248d2a860020116
dhanushvarman <110889258+dhanushvarman@users.noreply.github.com>
2022-09-01 15:09:04 +0530

    Update class.js

--- class.js
+++ class.js
@@ -1,16 +1,10 @@
 function foo(){
     var intial=document.getElementById("main").value;
     console.log(intial);
-}
-function foo1(){
     var intial1=document.getElementById("main1").value;
     console.log(intial1);
-}
-function foo2(){
     var intial2=document.getElementById("main2").value;
     console.log(intial2);
-}
-function foo3(){
     var intial3=document.getElementById("main3").value;
     console.log(intial3);
 }
